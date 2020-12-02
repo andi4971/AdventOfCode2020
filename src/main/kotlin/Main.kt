fb0afd1ebee0b9d90fd55515d1303ab7cbd3c1a7
andi4971 <andi4971@gmail.com>
2020-12-02 09:09:13 +0100

    Day 2 part 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,19 @@
 import java.io.File
 
 fun main(args: Array<String>) {
-    val lines = File("src/main/resources/input.txt").readLines().map { it.toInt() }
-    lines.forEach { nrOne ->
-        val multipliers1 = lines.filter { it !=nrOne  }
-        multipliers1.forEach{ nrTwo ->
-            val multipliers2 = multipliers1.filter {it != nrTwo}
-            multipliers2.forEach {
-                if(nrOne+nrTwo+it == 2020) println(nrOne*nrTwo*it)
-            }
-        }
+    val lines = File("src/main/resources/input.txt").readLines()
+    var correctPasswords = 0
+    lines.forEach { line ->
+        val split = line.split(':')
+        val policyParts = split[0].split(' ')
+        val policyOccurrence = policyParts[0].split('-').map { it.trim().toInt() }
+        val policyLetter = policyParts[1][0]
+        val content = split[1]
+        //9-10 m: mmmmnxmmmwm
+        val count = content.count { it == policyLetter }
+        if(count >= policyOccurrence[0] && count<= policyOccurrence[1]) correctPasswords++
     }
+    println(correctPasswords)
 
 }
 
