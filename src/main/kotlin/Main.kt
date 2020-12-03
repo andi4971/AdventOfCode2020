cfbbcafd84e1544cdf87d38b824d318ec4caf1aa
andi4971 <andi4971@gmail.com>
2020-12-03 10:00:52 +0100

    Day 3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,27 @@
 import java.io.File
 
 fun main(args: Array<String>) {
-    val lines = File("src/main/resources/input.txt").readLines()
-    var correctPasswords = 0
-    lines.forEach { line ->
-        val split = line.split(':')
-        val policyParts = split[0].split(' ')
-        val policyOccurrence = policyParts[0].split('-').map { it.trim().toInt() }
-        val policyLetter = policyParts[1][0]
-        val content = split[1].trimStart()
-        //9-10 m: mmmmnxmmmwm
+    val lines = File("src/main/resources/input.txt").readLines().map { it.toCharArray() }
+    var xOffsets = arrayOf(1,3,5,7,1)
+    var yOffsets = arrayOf(1,1,1,1,2)
+    val treeCounters = mutableListOf<Int>()
+    for (i in xOffsets.indices)
+    {
+        var treeCounter = 0
+        var x = 0
 
-        var count = 0
-        if(content[policyOccurrence[0]-1]==policyLetter) count++
-        if(content[policyOccurrence[1]-1]==policyLetter) count++
-
-        if(count==1) correctPasswords++
+        for(line in 0..lines.size -(1+yOffsets[i]) step yOffsets[i]) {
+            x+=xOffsets[i]
+            if(x >= lines[0].size)
+            {
+                x -= lines[0].size
+            }
+            if(lines[line+yOffsets[i]][x] == '#') treeCounter++
+        }
+        treeCounters.add(treeCounter)
     }
-    println(correctPasswords)
 
+    println(treeCounters.reduce{acc, i -> acc * i})
 }
 
 
