5fd3d7598c315fcfb8d783721ddfc8fbb6610321
andi4971 <andi4971@gmail.com>
2020-12-06 10:08:31 +0100

    Day 6

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,22 +2,19 @@ import java.io.File
 
 fun main(args: Array<String>) {
     val lines = File("src/main/resources/input.txt").readLines()
-    val seatIds = mutableListOf<Int>()
+            .joinToString { it }
+            .split(" ,")
+            .map { it.replace(",", "") }
+    var count = 0
     lines.forEach { line ->
-        val row = Integer.parseInt(line.substring(0,7).replace("F","0").replace("B","1"),2)
-        val seat = Integer.parseInt(line.substring(7).toString().replace("L","0").replace("R","1"),2)
-        val seatId = row * 8 + seat
-       seatIds.add(seatId)
-    }
-    seatIds.sort()
-    for(i in 0..seatIds.size-2)
-    {
-        if(seatIds[i+1]-seatIds[i] > 1) {
-            println(seatIds[i + 1])
-            println(seatIds[i])
+        val answers = line.trimStart().split(" ")
+        answers[0].forEach { answer ->
+            var contains = true;
+            answers.forEach { singleAnswers ->
+                if (!singleAnswers.contains(answer)) contains = false;
+            }
+            if (contains) count++
         }
     }
-
+    println(count)
 }
-
-
