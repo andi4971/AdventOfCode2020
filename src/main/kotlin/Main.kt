6144cb2cfcbf8b7fe171390a676c0b0c876ab6a3
andi4971 <andi4971@gmail.com>
2020-12-07 14:35:32 +0100

    Day 7

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,54 @@
+import org.jgrapht.Graph
+import org.jgrapht.GraphPath
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath
+import org.jgrapht.graph.*
 import java.io.File
 
 fun main(args: Array<String>) {
     val lines = File("src/main/resources/input.txt").readLines()
-            .joinToString { it }
-            .split(" ,")
-            .map { it.replace(",", "") }
-    var count = 0
+    val graph = SimpleDirectedWeightedGraph<String, DefaultWeightedEdge>(DefaultWeightedEdge::class.java)
+
+    //wavy green bags contain 1 posh black bags, 1 faded green bags, 4 wavy red bags
+
     lines.forEach { line ->
-        val answers = line.trimStart().split(" ")
-        answers[0].forEach { answer ->
-            var contains = true;
-            answers.forEach { singleAnswers ->
-                if (!singleAnswers.contains(answer)) contains = false;
-            }
-            if (contains) count++
+        val split = line.split("contain")
+        val mainVertexName = split[0].trim()
+        if (!graph.vertexSet().contains(mainVertexName)) {
+            graph.addVertex(mainVertexName)
+        }
+        if (!split[1].contains("no other bags")) {
+            val otherVertexes = split[1].split(",")
+            otherVertexes
+                    .map { it.trimStart() }
+                    .forEach {
+                        val cost = it.substring(0, 1).toDouble()
+                        val name = it.substring(1).trim()
+                        if (!graph.vertexSet().contains(name)) {
+                            graph.addVertex(name)
+                        }
+                        val edge = graph.addEdge(mainVertexName, name)
+                        graph.setEdgeWeight(edge, cost)
+                    }
+        }
+    }
+    var count = 0
+    graph.vertexSet().toList().forEach {
+        if (it != "shiny gold bags") {
+            if (DijkstraShortestPath.findPathBetween(graph, it, "shiny gold bags") != null) count++
         }
     }
     println(count)
+
+
+    println(calcWeight("shiny gold bags", graph))
+
+}
+
+fun calcWeight(vertex: String, graph: Graph<String, DefaultWeightedEdge>): Double {
+    var weight = 0.0
+    graph.outgoingEdgesOf(vertex).forEach {
+        weight += graph.getEdgeWeight(it)
+        weight +=  graph.getEdgeWeight(it)* calcWeight(graph.getEdgeTarget(it), graph)
+    }
+    return weight
 }
