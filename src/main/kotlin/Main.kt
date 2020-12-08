1abf0c96386c632af8d92574b355bd40b4bd5e62
andi4971 <andi4971@gmail.com>
2020-12-08 09:39:17 +0100

    Day 8

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,54 +1,50 @@
-import org.jgrapht.Graph
-import org.jgrapht.GraphPath
-import org.jgrapht.alg.shortestpath.DijkstraShortestPath
-import org.jgrapht.graph.*
 import java.io.File
 
 fun main(args: Array<String>) {
     val lines = File("src/main/resources/input.txt").readLines()
-    val graph = SimpleDirectedWeightedGraph<String, DefaultWeightedEdge>(DefaultWeightedEdge::class.java)
+        .map { it.split(" ") }
+    for (outerLoopCounter in lines.indices) {
+        if(lines[outerLoopCounter][0] == "acc") continue
 
-    //wavy green bags contain 1 posh black bags, 1 faded green bags, 4 wavy red bags
+        val copyList = lines.toMutableList()
+        var changed = false
 
-    lines.forEach { line ->
-        val split = line.split("contain")
-        val mainVertexName = split[0].trim()
-        if (!graph.vertexSet().contains(mainVertexName)) {
-            graph.addVertex(mainVertexName)
+        if (lines[outerLoopCounter][0] == "jmp") {
+            copyList[outerLoopCounter] = listOf("nop", lines[outerLoopCounter][1])
+            changed = true
         }
-        if (!split[1].contains("no other bags")) {
-            val otherVertexes = split[1].split(",")
-            otherVertexes
-                    .map { it.trimStart() }
-                    .forEach {
-                        val cost = it.substring(0, 1).toDouble()
-                        val name = it.substring(1).trim()
-                        if (!graph.vertexSet().contains(name)) {
-                            graph.addVertex(name)
-                        }
-                        val edge = graph.addEdge(mainVertexName, name)
-                        graph.setEdgeWeight(edge, cost)
-                    }
+        if(!changed && lines[outerLoopCounter][0] == "nop"){
+            copyList[outerLoopCounter] = listOf("jmp", lines[outerLoopCounter][1])
         }
+        val erg = runProgram(copyList)
+        erg?.let { println(it) }
     }
-    var count = 0
-    graph.vertexSet().toList().forEach {
-        if (it != "shiny gold bags") {
-            if (DijkstraShortestPath.findPathBetween(graph, it, "shiny gold bags") != null) count++
-        }
-    }
-    println(count)
-
-
-    println(calcWeight("shiny gold bags", graph))
-
 }
 
-fun calcWeight(vertex: String, graph: Graph<String, DefaultWeightedEdge>): Double {
-    var weight = 0.0
-    graph.outgoingEdgesOf(vertex).forEach {
-        weight += graph.getEdgeWeight(it)
-        weight +=  graph.getEdgeWeight(it)* calcWeight(graph.getEdgeTarget(it), graph)
+fun runProgram(lines: List<List<String>>): Int? {
+    var accumulator = 0
+    var i = 0
+    val visitedInstructions = mutableListOf<String>()
+    while (true) {
+        if(i==lines.size) return accumulator
+        var currLine = "$i ${lines[i][0]} ${lines[i][1]}"
+        if (visitedInstructions.contains(currLine)) {
+            return null
+        }
+        visitedInstructions.add(currLine)
+        when (lines[i][0]) {
+            "acc" -> {
+                val accInc = lines[i][1].toInt()
+                accumulator += accInc
+                i++
+            }
+            "jmp" -> {
+                val jumper = lines[i][1].toInt()
+                i += jumper
+            }
+            "nop" -> {
+                i++
+            }
+        }
     }
-    return weight
 }
