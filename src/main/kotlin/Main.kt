a8078cf4670faada48ffa22d521a79a0ac4172f0
andi4971 <andi4971@gmail.com>
2020-12-09 09:36:15 +0100

    Day 9 part 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,30 +4,17 @@ import kotlin.collections.ArrayDeque
 
 fun main(args: Array<String>) {
     val lines = File("src/main/resources/input.txt").readLines().map { it.toLong() }
-    val queue = ArrayDeque<Long>()
-    val initialLines = 25
-    val invalidNumber = 57195069
-    queue.addAll(lines.take(initialLines))
+    val invalidNumber = 57195069L
 
-    for (i in initialLines..lines.size) {
-        var currentNumber = lines[i]
-        val possibleProducts = mutableListOf<Long>()
-        queue.forEach { ele1 ->
-            queue.forEach { ele2 ->
-                if (ele1 != ele2) {
-                  val sum = ele1+ele2
-                    if(!possibleProducts.contains(sum))
-                    {
-                        possibleProducts.add(sum)
-                    }
-                }
+    for (i in lines.indices) {
+        var sum = 0L
+        val elements = mutableListOf<Long>()
+        for (j in i..lines.size-1) {
+            sum += lines[j]
+            elements.add(lines[j])
+            if (sum == invalidNumber) {
+                println(elements.minOrNull()!!.plus(elements.maxOrNull()!!))
             }
-        }
-        if(possibleProducts.contains(currentNumber)){
-            queue.add(currentNumber)
-            queue.removeFirst()
-        }else{
-            println(currentNumber)
         }
     }
 }
