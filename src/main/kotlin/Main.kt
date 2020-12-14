8a313a0d7fb1e99d8dfe47b3eb37679cd91c6dbe
andi4971 <andi4971@gmail.com>
2020-12-14 09:26:54 +0100

    Day 13 part 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,43 @@
 import java.io.File
-import kotlin.math.*
+import java.math.BigDecimal
+import java.math.BigInteger
+import java.math.RoundingMode
+import kotlin.math.ceil
 
 fun main(args: Array<String>) {
     var lines = File("src/main/resources/input.txt").readLines()
-    val arrTime = lines[0].toDouble()
-    val numbers = lines[1].split(',').mapNotNull { it.toDoubleOrNull() }
-
-    var min = Double.MAX_VALUE
-    var erg = 0.0
-    numbers.forEach {
-        val multiplier = ceil(arrTime / it)
-        val calc = (it * multiplier) - arrTime
-        if (calc < min) {
-            min = calc
-            erg = it * calc
+    val arrTime = lines[0].toBigInteger()
+    val numbers = lines[1].split(',')
+    var startVal: BigInteger
+    var multiplier = BigInteger.valueOf(5263157894736L)
+    while (true) {
+        var prevVal = numbers[0].toBigInteger() * multiplier
+        startVal = numbers[0].toBigInteger() * multiplier
+        println(startVal.toString())
+        var found = true
+        for (i in 1 until numbers.size) {
+            if (numbers[i] == "x") {
+                prevVal++
+                continue
+            }
+            val numb = numbers[i].toBigInteger()
+            //if ((prevVal + BigInteger.valueOf(1)) % numb == BigInteger.valueOf(0))
+            /*val multi = prevVal.div(numb).setScale(0, RoundingMode.CEILING)
+            val calc = (numb * multi) - prevVal*/
+            /*if (calc == BigInteger.valueOf(1L))*/
+            if ((prevVal + BigInteger.valueOf(1)) % numb == BigInteger.valueOf(0)) {
+                prevVal++
+                continue
+            } else {
+                found = false
+                break
+            }
         }
+        if (found) break
+
+
+        multiplier++
     }
-    println(erg)
+    println(startVal)
 
 }
