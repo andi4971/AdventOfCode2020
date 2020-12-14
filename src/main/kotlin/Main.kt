02f9704a2eefa26d316afb562b6da527ee6ff919
andi4971 <andi4971@gmail.com>
2020-12-14 12:09:13 +0100

    Day 14

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,43 +1,62 @@
 import java.io.File
-import java.math.BigDecimal
-import java.math.BigInteger
-import java.math.RoundingMode
-import kotlin.math.ceil
 
 fun main(args: Array<String>) {
     var lines = File("src/main/resources/input.txt").readLines()
-    val arrTime = lines[0].toBigInteger()
-    val numbers = lines[1].split(',')
-    var startVal: BigInteger
-    var multiplier = BigInteger.valueOf(5263157894736L)
-    while (true) {
-        var prevVal = numbers[0].toBigInteger() * multiplier
-        startVal = numbers[0].toBigInteger() * multiplier
-        println(startVal.toString())
-        var found = true
-        for (i in 1 until numbers.size) {
-            if (numbers[i] == "x") {
-                prevVal++
-                continue
+
+    val inputMap = mutableListOf<Pair<String, List<String>>>()
+
+    var currentMask = lines[0].substring(7)
+
+    var currentMems = mutableListOf<String>()
+    for (i in 1 until lines.size) {
+        if (lines[i].startsWith("mask")) {
+            inputMap.add(Pair(currentMask, currentMems))
+            currentMask = lines[i].substring(7)
+            currentMems = mutableListOf()
+        } else {
+            currentMems.add(lines[i])
+        }
+    }
+    inputMap.add(Pair(currentMask, currentMems))
+
+    val memMap = hashMapOf<Long, String>()
+    var count = 0
+    inputMap.forEach { input ->
+        input.second.forEach {
+            val split = it.split(" = ")
+            val address =
+                split[0].substring(4, split[0].length - 1).toLong().toString(2).padStart(36, '0').toCharArray()
+
+            val tempVal = split[1].toLong().toString(2)
+            val value = tempVal.padStart(36, '0').toCharArray()
+
+            for (i in input.first.indices) {
+                if (input.first[i] == '1') {
+                    address[i] = '1'
+                }
+                if (input.first[i] == 'X') {
+                    address[i] = 'X'
+                }
             }
-            val numb = numbers[i].toBigInteger()
-            //if ((prevVal + BigInteger.valueOf(1)) % numb == BigInteger.valueOf(0))
-            /*val multi = prevVal.div(numb).setScale(0, RoundingMode.CEILING)
-            val calc = (numb * multi) - prevVal*/
-            /*if (calc == BigInteger.valueOf(1L))*/
-            if ((prevVal + BigInteger.valueOf(1)) % numb == BigInteger.valueOf(0)) {
-                prevVal++
-                continue
-            } else {
-                found = false
-                break
+            val xCount = input.first.count { it == 'X' }
+            val possibilities = Math.pow(2.0,input.first.count { it == 'X' }.toDouble())
+            val possibleAddresses = mutableListOf<Long>()
+
+            for(i in 0..possibilities.toLong()){
+
+                val changeAddr = i.toString(2).padStart(xCount,'0')
+                var tempAddr = address.concatToString()
+                for(j in 0 until xCount){
+                    tempAddr = tempAddr.replaceFirst('X',changeAddr[j])
+                }
+                possibleAddresses.add(tempAddr.toLong(2))
+            }
+
+            possibleAddresses.forEach { addr ->
+                memMap[addr] = value.concatToString()
             }
         }
-        if (found) break
-
-
-        multiplier++
     }
-    println(startVal)
+    println(memMap.values.map { it.toLong(2) }.sum())
 
 }
