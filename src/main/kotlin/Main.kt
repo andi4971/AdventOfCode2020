2222a8393247603a7b0a6d51028b79ec45c35c47
andi4971 <andi4971@gmail.com>
2020-12-16 12:39:22 +0100

    Day 16 part 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,39 +1,35 @@
 import java.io.File
 
 fun main(args: Array<String>) {
-    var lines = File("src/main/resources/input.txt").readLines().first().split(',').map { it.toInt() }.toMutableList()
+    var lines = File("src/main/resources/input.txt").readLines()
 
-    var currentNumber = 0
-    var previousNumber = 0
-    val numberMap = hashMapOf<Int, Int>()
-
-    val calledNumbers = hashMapOf<Int, Int>()
-    for (turn in 1..30000000) {
-        previousNumber = currentNumber
-        if (lines.isNotEmpty()) {
-            currentNumber = lines.removeFirst()
-            numberMap[currentNumber] = turn
-            calledNumbers[currentNumber] = 1
-            continue
+    val fields = lines.takeWhile { it != "" }.associate {
+        val split = it.split(": ")
+        val name = split[0]
+        val ranges = split[1].split(" or ").map { rng ->
+            val range = rng.split("-")
+            return@map IntRange(range[0].toInt(), range[1].toInt())
         }
-        if (calledNumbers[previousNumber] == 1) {
-            //first time called
-            currentNumber = 0
-            if (calledNumbers.containsKey(currentNumber)) {
-                calledNumbers[currentNumber] = calledNumbers[currentNumber]!! + 1
-            }else{
-                calledNumbers[currentNumber] = 1
-            }
-        } else {
-            currentNumber = turn - 1 - numberMap[previousNumber]!!
-            if (calledNumbers.containsKey(currentNumber)) {
-                calledNumbers[currentNumber] = calledNumbers[currentNumber]!! + 1
-            }else{
-                calledNumbers[currentNumber] = 1
-            }
-        }
-        numberMap[previousNumber] = turn - 1
-        println((turn.toDouble() / 30000000.0) * 100)
+        name to (ranges[0] to ranges[1])
     }
-    println(currentNumber)
+
+
+    val myTicket = lines[fields.size + 2]
+
+    val nearbyTickets = lines.subList(fields.size + 5, lines.size).map { it.split(",").map { i -> i.toInt() } }
+    val removableTickets = mutableSetOf<List<Int>>()
+    nearbyTickets.forEach { ticket ->
+
+        for (i in ticket.indices) {
+            var validFields = 0
+            fields.forEach { field ->
+                if (field.value.first.contains(ticket[i]) || field.value.second.contains(ticket[i])) validFields++
+            }
+            if(validFields == 0){
+                removableTickets.add(ticket)
+            }
+        }
+    }
+    val validTickets = nearbyTickets - removableTickets
+    println(validTickets.size)
 }
