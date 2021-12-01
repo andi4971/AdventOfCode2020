6265ca9ed3b35440f0ef492dfc698eec07f06296
andi4971 <andi4971@gmail.com>
2021-12-01 08:28:22 +0100

    2021 Day 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,35 +1,18 @@
 import java.io.File
 
 fun main(args: Array<String>) {
-    var lines = File("src/main/resources/input.txt").readLines()
-
-    val fields = lines.takeWhile { it != "" }.associate {
-        val split = it.split(": ")
-        val name = split[0]
-        val ranges = split[1].split(" or ").map { rng ->
-            val range = rng.split("-")
-            return@map IntRange(range[0].toInt(), range[1].toInt())
-        }
-        name to (ranges[0] to ranges[1])
+    val lines = File("src/main/resources/input.txt").readLines()
+        .map { it.toInt() }
+    val sums = mutableListOf<Int>()
+    for(i in lines.indices) {
+        if(i+2 < lines.size)
+        sums += lines.subList(i, i+3).sum()
     }
-
-
-    val myTicket = lines[fields.size + 2]
-
-    val nearbyTickets = lines.subList(fields.size + 5, lines.size).map { it.split(",").map { i -> i.toInt() } }
-    val removableTickets = mutableSetOf<List<Int>>()
-    nearbyTickets.forEach { ticket ->
-
-        for (i in ticket.indices) {
-            var validFields = 0
-            fields.forEach { field ->
-                if (field.value.first.contains(ticket[i]) || field.value.second.contains(ticket[i])) validFields++
-            }
-            if(validFields == 0){
-                removableTickets.add(ticket)
-            }
-        }
+    var count = 0
+    for( i in 1 until sums.size) {
+        if(sums[i-1]<sums[i])
+            count ++;
     }
-    val validTickets = nearbyTickets - removableTickets
-    println(validTickets.size)
+    println(count)
+
 }
